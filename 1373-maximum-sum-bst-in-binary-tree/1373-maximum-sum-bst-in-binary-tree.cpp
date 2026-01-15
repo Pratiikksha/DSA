cbda1a04febd41e20d505247aef8f8f746a390b2
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-01-15 21:52:19 +0530

    Time: 0 ms (100%), Space: 104.2 MB (87.32%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxSum = 0;
+
+    struct Info {
+        bool isBST;
+        int sum;
+        int minVal;
+        int maxVal;
+    };
+
+    Info dfs(TreeNode* root) {
+        if (!root) {
+            return {true, 0, INT_MAX, INT_MIN};
+        }
+
+        Info left = dfs(root->left);
+        Info right = dfs(root->right);
+
+        // Check BST condition
+        if (left.isBST && right.isBST &&
+            root->val > left.maxVal &&
+            root->val < right.minVal) {
+
+            int currSum = root->val + left.sum + right.sum;
+            maxSum = max(maxSum, currSum);
+
+            return {
+                true,
+                currSum,
+                min(root->val, left.minVal),
+                max(root->val, right.maxVal)
+            };
+        }
+
+        // Not a BST
+        return {false, 0, 0, 0};
+    }
+
+    int maxSumBST(TreeNode* root) {
+        dfs(root);
+        return maxSum;
+    }
+};
