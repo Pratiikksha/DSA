1b58428a9d03a61e28f4d80c98822aad9f1b1e98
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-06-03 12:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (58.04%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isValid(int row, int col, vector<string>& board, int n) {
+        // Check upper column
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void solve(int row, vector<string>& board,
+               vector<vector<string>>& ans, int n) {
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(row, col, board, n)) {
+                board[row][col] = 'Q';
+
+                solve(row + 1, board, ans, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+
+        vector<string> board(n, string(n, '.'));
+
+        solve(0, board, ans, n);
+
+        return ans;
+    }
+};
