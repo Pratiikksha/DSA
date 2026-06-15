f8d2a2d16c3b150c923bebc15eae012514528c03
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-06-15 22:51:00 +0530

    Time: 60 ms (29.87%) | Memory: 93.9 MB (68.30%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -3,7 +3,6 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
          sort(intervals.begin(), intervals.end());
         int i =1;
-        vector<vector<int>> ans;
         int cnt=0;
         vector<int> temp = intervals[0];
         while(i < intervals.size()){
@@ -12,12 +11,10 @@ public:
                 temp[1]= min(temp[1], intervals[i][1]);
                 cnt++;
             }else{
-                ans.push_back({temp[0], temp[1]});
                 temp = intervals[i];
             }
             i++;
         }
-        ans.push_back(temp);
         return cnt;
     }
 };
