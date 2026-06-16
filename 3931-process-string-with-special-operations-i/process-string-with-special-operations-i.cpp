cc259d50e1a14c4eae2851073a88904df1639451
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-06-16 09:34:48 +0530

    Time: 1 ms (83.71%) | Memory: 39.8 MB (69.70%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    string reverse(string s){
-        string rev="";
-        for(char c:s){
-            rev=c+rev;
-        }
-        return rev;
-    }
+    // string reverse(string s){
+    //     string rev="";
+    //     for(char c:s){
+    //         rev=c+rev;
+    //     }
+    //     return rev;
+    // }
     string processStr(string s) {
         string st="";
         for(int i=0;i<s.size();i++){
             char c=s[i];
             if(c=='*'){
-                st=st.substr(0,st.size()-1);
+                if(!st.empty()) 
+                st.pop_back();
             }
             else if(c=='#'){
                 st=st+st;
             }
             else if(c=='%'){
-                st=reverse(st);
+                reverse(st.begin(),st.end());
             }
             else st=st+c;
         }
