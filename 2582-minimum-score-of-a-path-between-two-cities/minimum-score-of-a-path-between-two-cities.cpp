514e36d9690851d9be20dc4c15a65af12a138f11
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-07-04 10:42:39 +0530

    Time: 60 ms (58.89%) | Memory: 134.3 MB (53.80%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>>adj(n+1);
-        for(int i=0;i<roads.size();i++){
-            int u=roads[i][0];
-            int v=roads[i][1];
-            int wt=roads[i][2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (int i = 0; i < roads.size(); i++) {
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int wt = roads[i][2];
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        int ans=INT_MAX;
-        pq.push({1e7,1});
-        vector<int>scores(n+1,INT_MAX);
-        while(!pq.empty()){
-            int score=pq.top().first;
-            int node=pq.top().second;
+        queue<int> pq;
+        int ans = INT_MAX;
+        pq.push(1);
+        vector<int> vis(n + 1);
+        vis[1]=1;
+        while (!pq.empty()) {
+            int node = pq.front();
             pq.pop();
-            if(node==n){
-                ans=min(ans,score);
-            }
-            for(auto it:adj[node]){
-                int nScore=min(score,it.second);
-                if(nScore<scores[it.first]){
-                pq.push({nScore,it.first});
-                scores[it.first]=nScore;
+            for (auto it : adj[node]) {
+                ans = min(ans, it.second);
+                if (!vis[it.first]) {
+                    pq.push(it.first);
+                    vis[it.first]=1;
                 }
             }
         }
