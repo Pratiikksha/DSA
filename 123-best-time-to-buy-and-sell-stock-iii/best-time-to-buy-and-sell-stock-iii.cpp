2e6f5f7db793d35324bf340c9a1552f5dee824db
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-07-06 15:50:39 +0530

    Time: 401 ms (45.33%) | Memory: 211.5 MB (39.02%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,16 +4,6 @@ public:
         int n = prices.size();
         vector<vector<vector<int>>> dp(
             n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j <= 1; j++) {
-                dp[i][j][0] = 0;
-            }
-        }
-        for (int i = 0; i <= 1; i++) {
-            for (int j = 0; j <= 2; j++) {
-                dp[n][i][j] = 0;
-            }
-        }
         for (int i = n - 1; i >= 0; i--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int j = 1; j < 3; j++) {
