1bace83e04556b9f19dc9087e66049e4c3a75a9d
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-07-07 14:54:42 +0530

    Time: 43 ms (65.34%) | Memory: 18.6 MB (85.20%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int lcs(string a, string b) {
-        int n1 = a.size();
-        int n2 = b.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (a[i - 1] == b[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    bool check(string& a, string& b) {
+        if (a.size() != b.size() + 1)
+            return false;
+
+        int i = 0, j = 0;
+        while (i < a.size()) {
+            if (j < b.size() && a[i] == b[j]) {
+                i++;
+                j++;
+            } else {
+                i++;
             }
         }
-        return dp[n1][n2];
+        return j == b.size();
     }
     int longestStrChain(vector<string>& words) {
         sort(words.begin(), words.end(), [](const string& a, const string& b) {
@@ -23,10 +24,7 @@ public:
         int maxi = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (words[i].size() != words[j].size() &&
-                    words[i].size() == words[j].size() + 1 &&
-                    words[j].size() == lcs(words[i], words[j]) &&
-                    dp[i] < dp[j] + 1) {
+                if (check(words[i], words[j]) && dp[i] < dp[j] + 1) {
                     dp[i] = dp[j] + 1;
                 }
             }
