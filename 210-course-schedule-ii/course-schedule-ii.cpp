9ea8712bff2e56c9bd4294c154f01230758a3418
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-07-27 15:46:40 +0530

    Time: 7 ms (32.43%) | Memory: 18.9 MB (34.05%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> inDegree(numCourses,0);
-        for (int i = 0; i < prerequisites.size();i++) {
-            int a = prerequisites[i][0];
-            int b = prerequisites[i][1];
-            adj[b].push_back(a);
-            inDegree[a]++;
+        vector<vector<int>>adj(numCourses);
+        vector<int>inDegree(numCourses);
+        for(auto i:prerequisites){
+            adj[i[1]].push_back(i[0]);
+            inDegree[i[0]]++;
         }
-        queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
+        queue<int>q;
+        int vis=0;
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0){
                 q.push(i);
             }
         }
-        if (q.empty())
-            return {};
-        vector<int> ans;
-        vector<int> visited(numCourses, 0);
-        while (!q.empty()) {
-            int curr = q.front();
-            ans.push_back(curr);
-            visited[curr]=1;
+        if(q.empty()) return{};
+        vector<int>ans;
+        while(!q.empty()){
+            int sub=q.front();
+            ans.push_back(sub);
+            vis++;
             q.pop();
-            for (int nei : adj[curr]) {
-                if (!visited[nei]) {
-                    inDegree[nei]--;
-                    if (inDegree[nei] == 0)
-                        q.push(nei);
+            for(int i:adj[sub]){
+                inDegree[i]--;
+                if(inDegree[i]==0){
+                    q.push(i);
                 }
             }
         }
-        if(ans.size()!=numCourses) return {};
+        if(vis!=numCourses) return {};
         return ans;
     }
 };
