06bcdc7e576161a06cde2187d50da4a136caadfd
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-07-28 12:10:23 +0530

    Time: 7 ms (32.37%) | Memory: 11 MB (47.91%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     bool isValid(string s) {
-        string st=s;
-        int flag=1;
-        while(!st.empty()){
-            int n=st.size();
-            for(int i=0;i<n-2;i++){
-                string temp=st.substr(i,3);
-                if(temp=="abc"){
-                    flag=0;
-                    st=st.substr(0,i)+st.substr(i+3,n-i-2);
-                    break;
-                }
+        string st;
+        for (char c : s) {
+            st.push_back(c);
+            if (st.size() >= 3 && st.substr(st.size() - 3) == "abc") {
+                st.pop_back();
+                st.pop_back();
+                st.pop_back();
             }
-            if(flag && !st.empty()) return false;
-            flag=1;
         }
-        return true;
+        return st.empty();
     }
 };
