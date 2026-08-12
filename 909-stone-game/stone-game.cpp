96b67c3d3b6297fcf4050ce38b5a0d045b4c7a16
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-08-12 13:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (85.85%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        int sum=0;
-        int total=0;
-        for(int i=0;i<n/2;i++){
-            sum+=piles[i];
-            total+=piles[i];
-        }
-        int i=n/2;
-        int j=n-1;
-        int maxi=sum;
-        while(i>=0 && j<n/2){
-            sum=sum-piles[i]+piles[j];
-            maxi=max(maxi,sum);
-            total+=piles[j];
-            i--;
-            j--;
-        }
-        if(maxi>total/2) return true;
-        return false;
+        return true;
     }
 };
