81d51ba0a1970659969e55bcb9eb9057eef5e425
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-08-12 18:00:01 +0530

    Time: 18 ms (78.97%) | Memory: 73.9 MB (97.84%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         int n=nums.size();
-        priority_queue<pair<int,int>>mx;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>mn;
-        int r=0;
-        int l=0;
+        deque<int>mx;
+        deque<int>mn;
+        int r=0,l=0;
         int maxLen=1;
         while(r<n){
-            mx.push({nums[r],r});
-            mn.push({nums[r],r});
-            while(mx.top().first-mn.top().first > limit){
-                  l++;
-                 while(!mx.empty() && mx.top().second<l){
-                 mx.pop();
-                 }
-                while(!mn.empty() && mn.top().second<l){
-                mn.pop();
-                 }
+            while(!mx.empty() && nums[mx.back()]<=nums[r]){
+                mx.pop_back();
+            }
+            while(!mn.empty() && nums[mn.back()]>=nums[r]){
+                mn.pop_back();
+            }
+            mx.push_back(r);
+            mn.push_back(r);
+            while(nums[mx.front()]-nums[mn.front()]>limit && l<r){
+                l++;
+                while(!mx.empty() && mx.front()<l){
+                    mx.pop_front();
+                }
+                while(!mn.empty() && mn.front()<l){
+                    mn.pop_front();
+                }
             }
             maxLen=max(maxLen,r-l+1);
             r++;
