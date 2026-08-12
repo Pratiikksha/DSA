eb889f035c47824e6e088d33587cd88502cfcff3
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-08-12 18:45:58 +0530

    Time: 13 ms (76.81%) | Memory: 25.6 MB (40.51%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -3,7 +3,8 @@ public:
     int maxDistance(vector<vector<int>>& grid) {
         int n = grid.size();
         queue<pair<int, int>> q;
-        vector<vector<int>> dist(n, vector<int>(n, 1e9));
+        // vector<vector<int>> dist(n, vector<int>(n, 1e9));
+        int ans = INT_MAX;
         vector<vector<int>> vis(n, vector<int>(n, 0));
         int flag1 = false;
         int flag0 = false;
@@ -11,7 +12,6 @@ public:
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
                     q.push({i, j});
-                    dist[i][j] = 0;
                     flag1 = true;
                     vis[i][j] = 1;
                 } else {
@@ -23,30 +23,29 @@ public:
             return -1;
         int dx[] = {1, 0, -1, 0};
         int dy[] = {0, 1, 0, -1};
+         ans = -1;
+
         while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
-                    grid[nx][ny] == 0 && !vis[nx][ny]) {
-                    if (dist[x][y] + 1 < dist[nx][ny]) {
-                        dist[nx][ny] = dist[x][y] + 1;
-                        q.push({nx,ny});
-                        vis[nx][ny]=1;
+            int sz = q.size();
+            ans++;
+
+            while (sz--) {
+                auto [x, y] = q.front();
+                q.pop();
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
+                        grid[nx][ny] == 0) {
+
+                        grid[nx][ny] = 1;
+                        q.push({nx, ny});
                     }
                 }
             }
         }
-        int maxi = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                maxi = max(maxi, dist[i][j]);
-            }
-        }
-        return maxi;
+        return ans;
     }
-}
-;
+};
