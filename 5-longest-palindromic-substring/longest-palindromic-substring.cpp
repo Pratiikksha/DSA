71af6c664a63314cfdbc7ff1ef95a1ae0a5e6cd8
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-08-18 11:33:04 +0530

    Time: 7 ms (84.64%) | Memory: 9.5 MB (48.89%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
+    pair<int,int> expandAround(string& s, int l, int r){
+        int n=s.size();
+        while(l>=0 && r<n && s[l]==s[r]){
+            l--;
+            r++;
+        }
+        return {l+1,r-1};
+    }
     string longestPalindrome(string s) {
         int n=s.size();
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
-        for(int i=0;i<n;i++){
-            dp[i][i]=true;
-        }
+        int maxi=1;
         int start=0;
-         int maxi=1;
-        for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                dp[i][i+1]=true;
-                if(maxi<2){
-                        maxi=2;
-                        start=i;
-                    }
+        for(int i=0;i<n;i++){
+
+            auto [l1,r1]=expandAround(s,i,i);
+            int len=r1-l1+1;
+            if(maxi<len){
+                maxi=len;
+                start=l1;
             }
-        }
-        for(int i=3;i<=n;i++){
-            for(int j=0;j+i-1<n;j++){
-                if(s[j]==s[j+i-1] && dp[j+1][j+i-2]){
-                    dp[j][j+i-1]=true;
-                    if(maxi<i){
-                        maxi=i;
-                        start=j;
-                    }
-                }
+            auto [l2,r2]=expandAround(s,i,i+1);
+            len=r2-l2+1;
+            if(maxi<len){
+                maxi=len;
+                start=l2;
             }
         }
         return s.substr(start,maxi);
