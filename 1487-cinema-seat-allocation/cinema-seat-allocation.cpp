68485b92164badfe4944f6f795fadeaffa042d9e
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-08-21 10:28:38 +0530

    Time: 82 ms (25.90%) | Memory: 92.1 MB (8.46%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,31 +1,53 @@
 class Solution {
 public:
-    static int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        const int m=reservedSeats.size();
-        unordered_map<int, uint8_t> seat;
-        seat.reserve(m);
-        for(auto& r: reservedSeats){
-            const int i=r[0]-1, j=r[1]-2;
-            if (j<0 || j>=8) continue;
-            seat[i]|=1<<j;
+    int maxNumberOfFamilies(int k, vector<vector<int>>& reservedSeats) {
+        unordered_map<int, unordered_set<int>> mp;
+
+        // Store reserved seats row-wise
+        for (auto &v : reservedSeats) {
+            mp[v[0]].insert(v[1]);
         }
-        int sz=seat.size(), cnt=(n-sz)*2;
-        const uint8_t A=15, B=15<<2, C=15<<4, D=A|C;
-        for(auto [_, S]: seat){
-            S=~S;
-            bool has2=(S&D)==D, 
-            has1=(!has2)&& ((S&A)==A||(S&B)==B ||(S&C)==C);
-            cnt+=has2<<1;
-            cnt+=has1;
+
+        int ans = 0;
+
+        // Process only rows having reservations
+        for (auto &[row, seats] : mp) {
+            
+            bool left = true;   // 2,3,4,5
+            bool middle = true; // 4,5,6,7
+            bool right = true;  // 6,7,8,9
+
+            for (int seat = 2; seat <= 5; seat++) {
+                if (seats.count(seat)) {
+                    left = false;
+                    break;
+                }
+            }
+
+            for (int seat = 4; seat <= 7; seat++) {
+                if (seats.count(seat)) {
+                    middle = false;
+                    break;
+                }
+            }
+
+            for (int seat = 6; seat <= 9; seat++) {
+                if (seats.count(seat)) {
+                    right = false;
+                    break;
+                }
+            }
+
+            if (left && right)
+                ans += 2;
+            else if (left || middle || right)
+                ans += 1;
         }
-        return cnt;
+
+        // Rows with no reservations can always fit 2 families
+        int unaffectedRows = k - mp.size();
+        ans += unaffectedRows * 2;
+
+        return ans;
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
