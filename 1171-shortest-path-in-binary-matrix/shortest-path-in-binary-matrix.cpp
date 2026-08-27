5cdaf54ed7c925bb8be993fe3bd8ea4509e0fb96
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-08-27 09:59:39 +0530

    Time: 17 ms (57.55%) | Memory: 25.3 MB (55.23%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        queue<pair<int, pair<int, int>>> q;
-        if (grid[n - 1][n - 1] == 1 || grid[0][0] == 1)
+        int cnt = 0;
+        queue<pair<int, int>> q;
+        if (grid[0][0])
             return -1;
-        q.push({1, {0, 0}});
-        int dx[8]={0,1,-1,0,-1,1,1,-1};
-        int dy[8]={-1,0,0,1,1,1,-1,-1};
-        vector<vector<int>> vis(n,vector<int>(n,0));
-        vis[0][0]=1;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        vis[0][0] = 1;
+        q.push({0, 0});
+        int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+        int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
         while (!q.empty()) {
-            auto it = q.front();
-            int x=it.second.first;
-            int y=it.second.second;
-            int d=it.first;
-            q.pop();
-            if(x==n-1 && y==n-1) return d;
-            for(int i=0;i<8;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(nx<n && nx>=0 && ny<n && ny>=0 && !grid[nx][ny] && vis[nx][ny]==0){
-                    vis[nx][ny]=1;
-                    q.push({d+1,{nx,ny}});
+            int size = q.size();
+            cnt++;
+            while (size--) {
+                auto [x, y] = q.front();
+                if (x == n - 1 && y == n - 1)
+                    return cnt;
+                q.pop();
+                for (int i = 0; i < 8; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if (nx < n && ny < n && nx >= 0 && ny >= 0 &&
+                        grid[nx][ny] == 0 && !vis[nx][ny]) {
+                        q.push({nx, ny});
+                        vis[nx][ny] = 1;
+                    }
                 }
             }
         }
         return -1;
-
     }
 };
