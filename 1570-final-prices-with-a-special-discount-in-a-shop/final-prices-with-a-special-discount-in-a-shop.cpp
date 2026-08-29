0c34cc8aff5b051fe095d4c79df4f195fe713485
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-08-29 15:47:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (28.80%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,25 +3,25 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         stack<int> st;
         int n = prices.size();
-        vector<int> ans(n);
+        vector<int> ans=prices;
         for (int i = 0; i < n; i++) {
             while(!st.empty() && prices[st.top()] >= prices[i]) {
-                ans[st.top()] = i;
+                ans[st.top()] -= prices[i];
                 st.pop();
             }
             st.push(i);
         }
-        while (!st.empty()) {
-            ans[st.top()] = -1;
-            st.pop();
-        }
-        for (int i = 0; i < n; i++) {
-            if (ans[i] == -1) {
-                ans[i] = prices[i];
-            } else {
-                ans[i] = prices[i] - prices[ans[i]];
-            }
-        }
+        // while (!st.empty()) {
+        //     ans[st.top()] = -1;
+        //     st.pop();
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     if (ans[i] == -1) {
+        //         ans[i] = prices[i];
+        //     } else {
+        //         ans[i] = prices[i] - prices[ans[i]];
+        //     }
+        // }
         return ans;
     }
 };
