c475a3c74cfd911f8a34dd6538c712e3d773bbe7
Pratiikksha <155886100+Pratiikksha@users.noreply.github.com>
2026-09-07 13:31:40 +0530

    Time: 2 ms (72.66%) | Memory: 14.8 MB (44.21%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,10 +4,11 @@ public:
         unordered_map<int,int>mp;
         int n=nums.size();
         for(int i=0;i<n;i++){
-           if(mp.find(target-nums[i])!=mp.end()){
-            return {mp[target-nums[i]],i};
-           }
-           mp[nums[i]]=i;
+            int need=target-nums[i];
+            if(mp.count(need)){
+                return {i,mp[need]};
+            }
+            mp[nums[i]]=i;
         }
         return {};
     }
